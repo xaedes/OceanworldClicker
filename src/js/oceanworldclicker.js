0ecc623754cf20ad801c40c94e5cf2ac592daa31
xaedes <xaedes@gmail.com>
2014-06-23 23:58:22 +0200

    added plastic resource that can be gathered by resources that are nearby. when they are completely gathered you have to swim around to find more of them

--- src/js/oceanworldclicker.js
+++ src/js/oceanworldclicker.js
@@ -2,8 +2,11 @@
 function setInnerHTML(id,html) {
     document.getElementById(id).innerHTML = html;
 }
-function setFloat(id,flt) {
-    setInnerHTML(id,sprintf("%.2f",flt));
+function setFloat(id,value) {
+    setInnerHTML(id,sprintf("%.2f",value));
+}
+function setInt(id,value) {
+    setInnerHTML(id,sprintf("%d",value));
 }
 
 state = {};
@@ -13,16 +16,47 @@ state.water.max = 10;
 state.water.supplies = 1;
 state.water.suppliesEffect = 1;
 state.water.rate = 0;
+state.plastic = {};
+state.plastic.current = 0;
+state.plastic.nearby = 10;
 
 function updateWater() {
     setFloat("water",state.water.current);
     setFloat("waterMax",state.water.max);
-    setFloat("waterSupplies",state.water.supplies);
+    setInt("waterSupplies",state.water.supplies);
     setFloat("waterRate",state.water.rate);
+}
+function updatePlastic() {
+    setInt("plastic",state.plastic.current);
+    setInt("plasticNearby",state.plastic.nearby);
+}
+function updateResources() {
+    updatePlastic();
 }
 
 function updateAll() {
     updateWater();
+    updatePlastic();
+}
+
+function increment(variable, incr) {
+    if(variable.hasOwnProperty("nearby")) {
+        incr = Math.min(incr,variable.nearby);
+        variable.nearby -= incr;
+    }
+    variable.current += incr;
+    if(variable.hasOwnProperty("max")) {
+        variable.current = Math.min(variable.current, variable.max);
+    }
+}
+function decrement(variable, decr) {
+    increment(variable, -decr);
+}
+function random(min,max) {
+    return Math.random() * (max-min) + min;
+}
+function swim() {
+    state.plastic.nearby = random(10-5,10+5);
 }
 
 function loop() {
@@ -31,8 +65,7 @@ function loop() {
     state.water.rate -= 0.1;
     state.water.rate += 1 * state.water.suppliesEffect * state.water.supplies;
     // apply water rate
-    state.water.current += state.water.rate;
-    state.water.current = Math.min(state.water.current, state.water.max);
+    increment(state.water, state.water.rate)
     updateAll();
 }
 
