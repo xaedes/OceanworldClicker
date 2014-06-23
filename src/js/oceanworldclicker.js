9615e3971e5763ad32ff37db14de290ca6f333ff
xaedes <xaedes@gmail.com>
2014-06-24 01:37:14 +0200

    you can now build water supplies from plastic

--- src/js/oceanworldclicker.js
+++ src/js/oceanworldclicker.js
@@ -10,27 +10,39 @@ function setInt(id,value) {
 }
 
 state = {};
-state.water = {};
-state.water.current = 1;
-state.water.max = 10;
-state.water.supplies = 1;
-state.water.suppliesEffect = 1;
-state.water.rate = 0;
 state.plastic = {};
+state.plastic.name = "plastic";
 state.plastic.current = 0;
 state.plastic.nearby = 10;
+state.water = {};
+state.water.current = 1;
+state.water.current = 1;
+state.water.max = 10;
+state.water.supplies = {};
+state.water.supplies.current = 1;
+state.water.supplies.name = "supplies";
+state.water.supplies.effect = 1;
+state.water.supplies.build = [{'variable':state.water.supplies,'amount':1},{'variable':state.plastic,'amount':-10}];
+state.water.rate = 0;
+
 state.log = []
+// For the time now
+// http://stackoverflow.com/a/10211214/798588
+Date.prototype.timeNow = function () {
+     return ((this.getHours() < 10)?"0":"") + this.getHours() +":"+ ((this.getMinutes() < 10)?"0":"") + this.getMinutes() +":"+ ((this.getSeconds() < 10)?"0":"") + this.getSeconds();
+}
 
 function log(msg) {
     state.log.push(msg);
     console.log(msg);
-    document.getElementById("log").innerHTML += "<div>" + msg + "</div>";
+    date = new Date().timeNow()
+    document.getElementById("log").innerHTML = "<div>" + date + " " + msg + "</div>" + document.getElementById("log").innerHTML;
 }
 
 function updateWater() {
     setFloat("water",state.water.current);
     setFloat("waterMax",state.water.max);
-    setInt("waterSupplies",state.water.supplies);
+    setInt("waterSupplies",state.water.supplies.current);
     setFloat("waterRate",state.water.rate);
 }
 function updatePlastic() {
@@ -44,7 +56,7 @@ function updateResources() {
 function updateAll() {
     updateWater();
     updatePlastic();
-    updateLog();
+    // updateLog();
 }
 
 function increment(variable, incr) {
@@ -60,6 +72,21 @@ function increment(variable, incr) {
 function decrement(variable, decr) {
     increment(variable, -decr);
 }
+function build(recipe, n) {
+    var applicable = true;
+    for (var i = recipe.length - 1; i >= 0; i--) {
+        console.log(recipe[i]);
+        if (recipe[i].variable.current+recipe[i].amount<0){
+            applicable = false;
+            break;
+        }
+    }
+    if(applicable) {
+        for (var i = recipe.length - 1; i >= 0; i--) {
+            increment(recipe[i].variable,recipe[i].amount);
+        }
+    }
+}
 function random(min,max) {
     return Math.random() * (max-min) + min;
 }
@@ -72,7 +99,7 @@ function loop() {
     // calculate water rate
     state.water.rate = 0;
     state.water.rate -= 0.1;
-    state.water.rate += 1 * state.water.suppliesEffect * state.water.supplies;
+    state.water.rate += 1 * state.water.supplies.effect * state.water.supplies.current;
     // apply water rate
     increment(state.water, state.water.rate)
     updateAll();
