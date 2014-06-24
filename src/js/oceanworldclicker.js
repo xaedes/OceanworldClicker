f4b376eeb79a452b1caac31d701ca01830e75814
xaedes <xaedes@gmail.com>
2014-06-24 17:10:45 +0200

    added basic loading and saving with autosave every minute

--- src/js/oceanworldclicker.js
+++ src/js/oceanworldclicker.js
@@ -26,65 +26,102 @@ function setCost(id,build,ignore) {
 
 // Define data ====================================================================================
 state = {};
-state.sight = {};
-state.sight.current = 1;
-state.sight.variance = 1;
+function defineDefaultData(state) {
+    state.log = [];
+    state.autosave = {};
+    state.autosave.counter = {};
+    state.autosave.counter.current = 0;
+    state.autosave.counter.max = 60; // every minute
 
-state.population = {};
-state.population.current = 1;
-state.population.max = {};
-state.population.max.current = 2;
-state.population.waterConsumation = -0.1;
-state.population.findSurvivorProbability = 0.1;
+    state.sight = {};
+    state.sight.current = 1;
+    state.sight.variance = 1;
 
-state.plastic = {};
-state.plastic.name = "plastic";
-state.plastic.current = 0;
-state.plastic.nearby = {};
-state.plastic.nearby.current = 0;
-state.plastic.density = 10; 
+    state.population = {};
+    state.population.current = 1;
+    state.population.max = {};
+    state.population.max.current = 2;
+    state.population.waterConsumation = -0.1;
+    state.population.findSurvivorProbability = 0.1;
 
-
-state.water = {};
-state.water.current = 1;
-state.water.max = {};
-state.water.max.current = 10;
-
-state.water.rate = {};
-state.water.rate.current = 0;
-
-state.water.supplies = {};
-state.water.supplies.current = 1;
-state.water.supplies.name = "supplies";
-state.water.supplies.effect = 1;
-state.water.supplies.build = [{'variable':state.water.supplies,'amount':1},{'variable':state.plastic,'amount':-10}];
+    state.plastic = {};
+    state.plastic.name = "plastic";
+    state.plastic.current = 0;
+    state.plastic.nearby = {};
+    state.plastic.nearby.current = 0;
+    state.plastic.density = 10; 
 
 
-state.water.reservoirs = {};
-state.water.reservoirs.name = "Water reservoir";
-state.water.reservoirs.current = 1;
-state.water.reservoirs.effect = 10;
-state.water.reservoirs.build = [{'variable':state.water.reservoirs,'amount':1},{'variable':state.plastic,'amount':-10}];
+    state.water = {};
+    state.water.current = 1;
+    state.water.max = {};
+    state.water.max.current = 10;
+
+    state.water.rate = {};
+    state.water.rate.current = 0;
+
+    state.water.supplies = {};
+    state.water.supplies.current = 1;
+    state.water.supplies.name = "supplies";
+    state.water.supplies.effect = 1;
+
+
+    state.water.reservoirs = {};
+    state.water.reservoirs.name = "Water reservoir";
+    state.water.reservoirs.current = 1;
+    state.water.reservoirs.effect = 10;
+}
+defineDefaultData(state);
+
+// Define buildings  ==============================================================================
+function defineBuilds(state) {
+    state.water.supplies.build = [{'variable':state.water.supplies,'amount':1},{'variable':state.plastic,'amount':-10}];
+    state.water.reservoirs.build = [{'variable':state.water.reservoirs,'amount':1},{'variable':state.plastic,'amount':-10}];
+    return state;
+}
+
 
 // Define calculations  ===========================================================================
-old = function(val) {return val;};
-zero = function(val) {return 0;};
-add_to = function(add_variable,add_weight,val) {return val+getValue(add_variable)*getValue(add_weight)};
-add_gaussian = function(variance_variable,scale_variable,val) {return val+gaussian(getValue(variance_variable)*getValue(scale_variable))};
+function defineCalculations(state) {
+    old = function(val) {return val;};
+    zero = function(val) {return 0;};
+    value = function(value, val) {return 0;};
+    add_to = function(add_variable,add_weight,val) {return val+getValue(add_variable)*getValue(add_weight)};
+    incr = function(val) {return val+1;};
+    decr = function(val) {return val-1;};
+    add_gaussian = function(variance_variable,scale_variable,val) {return val+gaussian(getValue(variance_variable)*getValue(scale_variable))};
+    modulo = function(div,val) {return val % getValue(div)};
 
-state.water.rate.calculate = zero; // begin with zero (omit old value)
-state.water.rate.calculate = _.compose(_.partial(add_to, state.population, state.population.waterConsumation), state.water.rate.calculate);
-state.water.rate.calculate = _.compose(_.partial(add_to, state.water.supplies, state.water.supplies.effect), state.water.rate.calculate);
+    state.water.rate.calculate = zero; // begin with zero (omit old value)
+    state.water.rate.calculate = _.compose(_.partial(add_to, state.population, state.population.waterConsumation), state.water.rate.calculate);
+    state.water.rate.calculate = _.compose(_.partial(add_to, state.water.supplies, state.water.supplies.effect), state.water.rate.calculate);
 
-state.water.max.calculate = zero; // begin with zero (omit old value)
-state.water.max.calculate = _.compose(_.partial(add_to, state.water.reservoirs, state.water.reservoirs.effect), state.water.max.calculate);
+    state.water.max.calculate = zero; // begin with zero (omit old value)
+    state.water.max.calculate = _.compose(_.partial(add_to, state.water.reservoirs, state.water.reservoirs.effect), state.water.max.calculate);
 
-state.water.calculate = old;
-state.water.calculate = _.compose(_.partial(add_to, state.water.rate, 1), state.water.calculate);
+    state.water.calculate = old;
+    state.water.calculate = _.compose(_.partial(add_to, state.water.rate, 1), state.water.calculate);
 
-state.plastic.nearby.calculate = zero;
-state.plastic.nearby.calculate = _.compose(_.partial(add_to, state.sight, state.plastic.density), state.plastic.nearby.calculate);;
-state.plastic.nearby.calculate = _.compose(_.partial(add_gaussian, state.sight.variance, state.plastic.density), state.plastic.nearby.calculate);;
+    state.plastic.nearby.calculate = zero;
+    state.plastic.nearby.calculate = _.compose(_.partial(add_to, state.sight, state.plastic.density), state.plastic.nearby.calculate);
+    state.plastic.nearby.calculate = _.compose(_.partial(add_gaussian, state.sight.variance, state.plastic.density), state.plastic.nearby.calculate);
+
+    state.autosave.counter.calculate = old;
+    state.autosave.counter.calculate = _.compose(incr, state.autosave.counter.calculate);
+    state.autosave.counter.calculate = _.compose(_.partial(modulo, state.autosave.counter.max), state.autosave.counter.calculate);
+
+    return state;
+}
+
+
+function defineNonData(state) {
+    defineBuilds(state);
+    defineCalculations(state);
+
+    return state;
+}
+
+defineNonData(state);
 
 // Logging ========================================================================================
 state.log = []
@@ -93,6 +130,47 @@ state.log = []
 Date.prototype.timeNow = function () {
      return ((this.getHours() < 10)?"0":"") + this.getHours() +":"+ ((this.getMinutes() < 10)?"0":"") + this.getMinutes() +":"+ ((this.getSeconds() < 10)?"0":"") + this.getSeconds();
 }
+
+// Load & Save ====================================================================================
+function sanitize_for_serialization(variable) {
+    // omit non data keys
+    var sanitized = _.omit(variable, 'calculate', 'build');
+    // recursive sanitization
+    _.chain(_.keys(variable))
+        .each(function(key){
+            if(_.isObject(sanitized[key]) && !_.isArray(sanitized[key])) {
+                sanitized[key] = sanitize_for_serialization(sanitized[key]);
+            }
+        });
+
+    return sanitized;
+}
+function serialize(state) {
+    var sanitized = sanitize_for_serialization(state);
+    var json = JSON.stringify(sanitized);
+    var compressed = LZString.compressToBase64(json);
+    return compressed;
+}
+function deserialize(compressed) {
+    var state = {};
+    if(_.isNull(compressed) || _.isUndefined(compressed) || compressed==="undefined") {
+        defineDefaultData(state);
+    } else {
+        var json = LZString.decompressFromBase64(compressed);
+        state = JSON.parse(json);
+    }
+
+    return defineNonData(state);
+}
+
+function load() {
+    state = deserialize(localStorage.getItem('state'));
+}
+function save() {
+    localStorage.setItem('state', serialize(state));
+
+}
+load();
 
 function log(msg) {
     state.log.push(msg);
@@ -155,7 +233,7 @@ function setValue(variable, value) {
     if(variable.hasOwnProperty("current")) {
         variable.current = value;
         if(variable.hasOwnProperty("max")) {
-            variable.current = Math.min(variable.current, variable.max.current);
+            variable.current = Math.min(variable.current, getValue(variable.max));
         }    
     }
     return variable;
@@ -209,10 +287,18 @@ function apply_calculate(variable) {
 
 // game loop ======================================================================================
 function loop() {
+    // Autosave
+    if(getValue(state.autosave.counter)==0) {
+        log("Autosave");
+        save();
+    }
+    // Calculations
     apply_calculate(state.water.max);
     apply_calculate(state.water.rate);
     apply_calculate(state.water);
+    apply_calculate(state.autosave.counter);
 
+    // Display
     displayAll();
 }
 
