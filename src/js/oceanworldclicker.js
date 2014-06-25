f316d8e9115d3506e0c79c6bcadf2baf618138e8
xaedes <xaedes@gmail.com>
2014-06-25 20:07:17 +0200

    sth with savegames

--- src/js/oceanworldclicker.js
+++ src/js/oceanworldclicker.js
@@ -276,7 +276,7 @@ Date.prototype.timeNow = function () {
 // Load & Save ====================================================================================
 function sanitize_for_serialization(variable) {
     // omit non data keys
-    var sanitized = _.omit(variable, 'calculate', 'build','log');
+    var sanitized = _.omit(variable, 'calculate', 'build', 'log', 'calculate_swim', 'max', 'min','effect');
     // recursive sanitization
     _.chain(_.keys(variable))
         .each(function(key){
@@ -342,22 +342,28 @@ function version_upgrade(state) {
             // FALLTHROUGH       
         case 1:
             // new values for:
-            delete state.plastic.density;
-            delete state.planks.density;
-            delete state.sight.lookout.effect;
-
-            state.version = 2;
+            try {
+                delete state.plastic.density;
+                delete state.planks.density;
+                delete state.sight.lookout.effect;
+            } finally {
+                state.version = 2;
+            }
             // FALLTHROUGH       
         case 2:
             // new values for:
-            delete state.population.waterConsumation;
-
-            state.version = 3;
+            try {
+                delete state.population.waterConsumation;
+            } finally {
+                state.version = 3;
+            }
             // FALLTHROUGH       
         case 3:
-            delete state.log;
-
-            state.version = 4;
+            try {
+                delete state.log;
+            } finally {
+                state.version = 4;
+            }
             break;     
         default:
             log(sprintf("Unknown version: %d", stateVersion));
