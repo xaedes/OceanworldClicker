4122b4a03f211a35552527a0ea32f1b4f8e9321c
xaedes <xaedes@gmail.com>
2014-07-10 11:05:29 +0200

    added default format for display component

--- src/js/game/components/display.js
+++ src/js/game/components/display.js
@@ -2,7 +2,7 @@ define(['ash'], function (Ash) {
     var Display = Ash.Class.extend({
         format: null,
         constructor: function (format) {
-            this.format = format;
+            this.format = format == null ? "%d" : format;
         }
     });
 
